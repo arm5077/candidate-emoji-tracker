a93154c8668f6ccf308ad638a50a9394e471c55a
Andrew McGill <arm5077@gmail.com>
2015-12-14 17:31:14 -0500

    Refactor streaming code into own module; add feed frontend

--- public/app.js
+++ public/app.js
@@ -1,5 +1,11 @@
-angular.module("app", [])
+angular.module("app", ['ngAnimate'])
 .controller("controller", ["$scope", "$http", "$sce", function($scope, $http, $sce){
+	
+	$scope.feed = [];
+	$http.get("/api/latest").success(function(data){
+		$scope.feed = data;
+	});
+	
 	
 	$http.get("candidates.json").success(function(data){
 		
@@ -12,6 +18,15 @@ angular.module("app", [])
 	
 	$http.get("/api/list").success(function(data){
 		$scope.data = data;
+	});
+	
+	
+	var socket = io();
+	socket.on('tweet', function (data) {
+		$scope.$apply(function(){
+			$scope.feed.unshift(data);
+		})
+		console.log(data);
 	});
 	
 }]);
