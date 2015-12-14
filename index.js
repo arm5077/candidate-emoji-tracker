eab4b4a80d2edb9a8d2e2371936398ca2da876d5
Andrew McGill <arm5077@gmail.com>
2015-12-14 17:31:14 -0500

    Refactor streaming code into own module; add feed frontend

--- index.js
+++ index.js
@@ -1,39 +1,23 @@
 var express = require("express");
 var app = express();
-var mysql = require("mysql");
+var server = require('http').Server(app);
+var api = require("api");
+var politicalEmojis = require("political-emojis");
 
-// Create pool of connections
-var pool = mysql.createPool((process.env.CLEARDB_DATABASE_URL || "mysql://root@localhost/candidate-emoji-tracker") + "?debug=true&charset=utf8mb4");
-pool.on("error", function(err){  
-	console.log(err);
-	pool.end;
-});
 
 // Start web server 
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
-	console.log("We're live at port " + port + ".");
+
+server.listen(port, function(){
+	console.log("we're ready to roll!")
 });
+
+app.use('/api', api)
+	.use('/stream', politicalEmojis.app);
 
 // Set up static page (main page)
 app.use("/", express.static(__dirname + "/public/"));
 
-// Endpoint to get full emoji list per candidate
-app.get("/api/list", function(request, response){	
-	pool.query('SELECT candidate, emoji, COUNT(emoji) as count FROM emoji_counts GROUP BY candidate, emoji ORDER BY candidate ASC, count DESC', function(err, rows){ 
-		if(err) throw err; 
-		
-		var candidates = [];
-		
-		rows.forEach(function(row){
-			var index = candidates.map(function(d){ return d.name }).indexOf(row.candidate);
-			if( index == -1 ){
-				candidates.push({ name: row.candidate, emojis: [] });
-				index = candidates.length - 1;
-			}
-			candidates[index].emojis.push({ emoji: row.emoji, count: row.count })
-			
-		});
-		response.status(200).json(candidates);
-	});
-});
+
+politicalEmojis.acceptConnections(server);
+politicalEmojis.filterPoliticalEmojis();
