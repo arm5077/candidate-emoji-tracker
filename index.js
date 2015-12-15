8354bd50e182d7feb1b4ecbe9b54bbb146295d1f
Andrew McGill <arm5077@gmail.com>
2015-12-15 00:29:05 -0500

    Upgrades to pooling

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var app = express();
 var server = require('http').Server(app);
+var mysql = require("mysql");
 var api = require("api");
 var politicalEmojis = require("political-emojis");
+
 
 // Start web server 
 var port = process.env.PORT || 3000;
@@ -11,8 +13,16 @@ server.listen(port, function(){
 	console.log("we're ready to roll!")
 });
 
+// Create pool of connections
+global.pool = mysql.createPool((process.env.CLEARDB_DATABASE_URL || "mysql://root@localhost/candidate-emoji-tracker") + "?debug=true&charset=utf8mb4");
+pool.on("error", function(err){  
+	console.log(err);
+	pool.end;
+});
+
 app.use('/api', api)
 	.use('/stream', politicalEmojis.app);
+
 
 // Set up static page (main page)
 app.use("/", express.static(__dirname + "/public/"));
